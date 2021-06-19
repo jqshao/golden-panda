d4f257183b08afbf11940bb4a43ab6fc01bdd13d
jqshao <jqshao12@gmail.com>
2021-06-18 22:36:06 -0400

    home screen, nav bar fixed, starting menu

--- assets/script.js
+++ assets/script.js
@@ -2,14 +2,11 @@
 			Menu Toggle
 *********************************/
 
-const menuToggle = document.getElementById("menu-bar");
-const hamburgerNav = document.querySelector(".navbar");
+$(document).ready(function(){
 
-menuToggle.addEventListener("click", function toggleVisibility() {
-	if (hamburgerNav.style.visibility == "hidden") {
-		hamburgerNav.style.visibility = "visible";
-	} else {
-		hamburgerNav.style.visibility = "hidden";
-	}
+/*click menu-bar, change styling from class navbar to nav-toggle*/
+	$('#menu-bar').click(function(){
+		$('.navbar').toggleClass('nav-toggle');
+	})
+
 })
-
